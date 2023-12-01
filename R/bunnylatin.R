6c0e05a839c1861f56c0645a55c8cb55081e839f
April Hwang <april.hwang@ubc.ca>
2023-12-01 11:43:17 -0800

    update

--- R/bunnylatin.R
+++ R/bunnylatin.R
@@ -1,3 +1,19 @@
+#' Bunny Latin
+#'
+#' @details
+#' Bunny Latin will move the last two letters of the word to the front, then repeat the last letter of the newly modified word once at the end.
+#'
+#' @param x A string, or strings
+#'
+#' @return A modified string, according to Bunny Latin Rules
+#' @export
+#'
+#' @examples
+#' bunnylatin('apple')
+#'
+#' x <- c('summer', 'sunshine', 'puppy')
+#' bunnylatin(x)
+#'
 bunnylatin <- function(x) {
   last_two <- str_sub(x, -2, -1)
   first <- str_sub(x, 1, -3)
