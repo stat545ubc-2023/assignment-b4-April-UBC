3d0026ff375cdd091a56503818acab46c6c51a3e
April Hwang <april.hwang@ubc.ca>
2023-12-01 17:06:21 -0800

    update

--- tests/testthat/test-bunnylatin.R
+++ tests/testthat/test-bunnylatin.R
@@ -2,11 +2,11 @@ test_that('bunnylatin() moves last two letters to front and repeat last letter',
   expect_equal(bunnylatin('pepper'), 'erpeppp')
 })
 
-test_that('bunnylatin() errors if input is not string or vector', {
+test_that('bunnylatin() errors if input is not a string or vector', {
   expect_error(bunnylatin(notastring))
 })
 
 
-test_that('bunnylatin() errors if word is less than three letters', {
+test_that('bunnylatin() errors if word contains less than three letters', {
   expect_error(bunnylatin('ga'))
 })
